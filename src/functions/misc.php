465db5deb813ea0a1d89ee12fe1b8db7e44079a7
Derrick Hammer <derrick@derrickhammer.com>
2021-11-08 10:30:03 -0500

    *Add utility function to convert entity objects to array

--- src/functions/misc.php
+++ src/functions/misc.php
@@ -2,6 +2,8 @@
 
 namespace Skynet\functions\misc;
 
+use Skynet\Entity;
+
 function arrayToObject( $data ) {
 	if ( ! is_array( $data ) ) {
 		return $data;
@@ -12,4 +14,24 @@ function arrayToObject( $data ) {
 	}
 
 	return (object) array_map( __FUNCTION__, $data );
+}
+
+function entityToArray( Entity $entity ): array {
+	$data = $entity->toArray();
+
+	foreach ( $data as $name => $item ) {
+		if ( $item instanceof Entity ) {
+			$data[ $name ] = entityToArray( $item );
+		}
+
+		if ( is_array( $item ) ) {
+			foreach ( $item as $index => $subItem ) {
+				if ( $subItem instanceof Entity ) {
+					$data[ $name ][ $index ] = entityToArray( $subItem );
+				}
+			}
+		}
+	}
+
+	return $data;
 }
